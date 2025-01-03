07440f180d48c067cb8da81e5cdf32055e463c4f
Furkan Özyurt <123460078+FurkannOzyurt@users.noreply.github.com>
2025-01-03 19:07:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,41 +1,44 @@
 from setuptools import setup, find_packages
 
-setup(name='anfis',
-      version='0.3.1',
-      description='Python Adaptive Neuro Fuzzy Inference System',
-      url='https://github.com/twmeggs/anfis',
-	  download_url = 'https://github.com/twmeggs/anfis/tarball/0.3.1',
-      author='Tim Meggs',
-      author_email='twmeggs@gmail.com',
-      license='MIT',
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-      package_data = {
-        # If any package contains *.txt files, include them:
-        '': ['*.txt']},
-
-      keywords = 'anfis, fuzzy logic, neural networks',
-
-      packages=find_packages(),
-
-      install_requires = ['numpy','matplotlib','scikit-fuzzy'],
-
-      classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',]
+setup(
+    name="anfis",
+    version="0.3.1",
+    description="Python Adaptive Neuro Fuzzy Inference System",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/twmeggs/anfis",
+    author="Tim Meggs",
+    author_email="twmeggs@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "scikit-fuzzy",
+    ],
+    keywords=[
+        "anfis",
+        "fuzzy logic",
+        "neural networks",
+        "adaptive systems",
+        "machine learning",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.6",
 )
+
